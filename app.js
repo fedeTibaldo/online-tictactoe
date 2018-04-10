dbfaada207f5dabdc45f6ac6d1f2e862e69255dc
fedeTibaldo <tibaldo125@gmail.com>
2018-04-10 11:14:17 +0200

    Modify app url structure

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
-const path = require('path')
-
-class Room {
-    constructor(id) {
-        this.id = id
-        this.players = 0
-    }
-    addPlayer() {
-        if (this.players < 2)
-            return this.players++ == 0 ? 'X' : 'O'
-        return false
-    }
-}
-
-let PROGRESSIVE_ID = 0
-
-let private = []
-let public = []
-
-function getRoom(n) {
-    console.log(private)
-    for (let room of private.concat(public)) {
-        if (room.id === n)
-            return room
-    }
-    return false
-}
-
+/* ExpressJS */
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
+const server = require('http').Server(app)
+/* Socket.IO */
+const io = require('socket.io')(server)
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+function getRefererFromRequest(request, siteURL) {
+    return request.headers.referer.replace(siteURL, '')
+}
 
 app.use('/static', express.static('assets'))
-
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -43,34 +18,24 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/new', (req, res) => {
-    res.render('new', {
-        pageTitle: 'New Room @ Online TicTacToe'
+app.get('/join', (req, res) => {
+    res.render('join', {
+        pageTitle: 'Join Game @ Online TicTacToe'
     })
 })
 
-app.post('/new', (req, res) => {
-    let type = req.body.type === 'private' ? 'private' : 'public'
-    let room = new Room(PROGRESSIVE_ID)
-    if (type == 'private')
-        private.push( room )
-    else
-        public.push( room )
-    res.redirect(`/room/${type}/${PROGRESSIVE_ID++}`)
+app.get('/play', (req, res) => {
+    res.render('game', {
+        pageTitle: 'Play @ Online TicTacToe',
+    })
 })
 
-app.get('/room/private/:roomId', (req, res) => {
-    let room = getRoom(req.params.roomId)
-    if (!room)
-        res.sendStatus('404')
-    let glyph = room.addPlayer()
-    if (!glyph)
-        res.sendStatus('403')
-    res.cookie('room', { id: req.params.roomId, glyph: glyph }, { maxAge: 86400000 })
-})
+io.on('connection', function(socket){
+    console.log(`a user connected`);
+    console.log(getRefererFromRequest(socket.request, 'http://localhost'));
+    socket.on('disconnect', (reason) => {
+        console.log(`a user disconnected`);
+    });
+});
 
-app.get('/room/full', (req, res) => {
-    res.sendFile( path.resolve( __dirname, '') )
-})
-
-app.listen(80, () => console.log('Example app listening on port 80!'))
+server.listen(80, () => console.log('Example app listening on port 80!'))
